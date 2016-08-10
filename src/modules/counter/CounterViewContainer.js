7af0dff9725f630aa0b763c47f1adcf2322aac06
tehmou <timo.tuominen@gmail.com>
2016-08-10 12:57:48 +0100

    Move data into the state

--- src/modules/counter/CounterViewContainer.js
+++ src/modules/counter/CounterViewContainer.js
@@ -3,6 +3,7 @@ import CounterView from './CounterView';
 
 export default connect(
   state => ({
+    pokemonList: state.getIn(['counter', 'pokemonList']),
     selectedPokemon: state.getIn(['counter', 'selectedPokemon']),
     calculatedCp: state.getIn(['counter', 'calculatedCp']),
   })
